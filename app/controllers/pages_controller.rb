9a72349ea94c6f6c800dcaead71330153e3fdee2
Daniel Landes <daniellandes@gmail.com>
2019-12-10 17:26:40 +0100

    FEAT: Added part of share page and moved text to text.scss file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def share
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/share', to: 'pages#share'
+  get 'howitworks', to: 'pages#howitworks'
+  resources :booking, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
