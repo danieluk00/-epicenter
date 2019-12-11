9bc68fd0577903a1c60040e73d8ffcd36923e52c
Miguel Angel Marfil Rubio <mianmaru@gmail.com>
2019-12-11 15:22:01 +0100

    creating events with users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,15 +6,27 @@ class EventsController < ApplicationController
   end
 
   def create
-
-    @event = Event.new()
+    @event = Event.new(secure_params_event)
+    @event.save
+    @user = User.new(secure_params_user)
+    @user.event = @event
+    @user.organiser = true
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def secure_params
-    params.require(:event).permit(:event_name, :start_dt, :registration_deadline)
-    params.require(:user).permit(:name, :addres)
+  def secure_params_event
+    params.require(:event).permit(:event_name, :start_dt, :registration_deadline, :registration_deadline)
+  end
+
+  def secure_params_user
+    params_sec = params.require(:event).permit(user: [:name, :address])
+    params_sec[:user]
   end
 
 end
