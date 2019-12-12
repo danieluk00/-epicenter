fa0155f74624b9c0e6474d59ca78a70b1d43fa3a
Gabriel Ursache <gabriel@markov.ro>
2019-12-12 16:56:06 +0100

    adding map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def index
+    @users = User.geocoded
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   has_many :users
+  
 end
 
