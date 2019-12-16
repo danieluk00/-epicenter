c08566adc7d80b349392514ab58bc6da654aa1ee
Miguel Angel Marfil Rubio <mianmaru@gmail.com>
2019-12-16 16:41:41 +0100

    checked the icon current location in joining page and require params venue_type in new event form

--- app/javascript/packs/get_current_location.js
+++ app/javascript/packs/get_current_location.js
@@ -6,16 +6,15 @@ const autocompleteAddress = () => {
       navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
-        const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1IjoibWlhbm1hcnUiLCJhIjoiY2szMDl2MnZsMDMzNDNtbm45eGt4eXZociJ9.YnUj-3tl7H3JgmMzmQWU3w`
+        const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1IjoibWlhbm1hcnUiLCJhIjoiY2szMDl2MnZsMDMzNDNtbm45eGt4eXZociJ9.YnUj-3tl7H3JgmMzmQWU3w`;
         fetch(url)
           .then(response => response.json())
           .then((data) => {
             const currentAddress = data.features[0].place_name
             const addressField = document.getElementById("event_user_address");
             const addressField2 = document.getElementById("user_address");
-
-            console.log(addressField2)
-
+            // We console log the current address to inspect in heroku if we get it
+            console.log(currentAddress);
             if (addressField) {
                 addressField.value = currentAddress;
             } else if (addressField2)  {
