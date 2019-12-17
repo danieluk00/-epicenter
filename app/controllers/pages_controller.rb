ce75325c22692ba2c3c02d25487763f98a5a4aa5
Miguel Angel Marfil Rubio <mianmaru@gmail.com>
2019-12-17 11:17:54 +0100

    list of invitees in everywhere, we need to format the bullets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,5 +19,7 @@ class PagesController < ApplicationController
   def waiting
     @event = Event.find_by(event_token: params[:event])
     @organiser = User.find_by(event: @event, organiser: true)
+    # we want to show all the confirmed users for this event
+    @users = User.where(event: @event)
   end
 end
