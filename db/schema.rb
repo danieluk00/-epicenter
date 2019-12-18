060cafa34c7e5e55ba55ee12e0b4282d40c58cac
Miguel Angel Marfil Rubio <mianmaru@gmail.com>
2019-12-18 16:41:29 +0100

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_114804) do
+ActiveRecord::Schema.define(version: 2019_12_18_154040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2019_12_18_114804) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
-    t.boolean "included"
+    t.boolean "included_in_epicenter"
     t.index ["event_id"], name: "index_users_on_event_id"
   end
 
